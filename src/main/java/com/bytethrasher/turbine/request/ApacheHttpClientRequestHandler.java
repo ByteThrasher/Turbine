a752e7893c60cd8777119eecc9de02236a6ba330
laxika <laxika91@gmail.com>
2024-08-04 01:24:58 +0200

    #1 - Added WARC writing.

--- src/main/java/com/bytethrasher/turbine/request/ApacheHttpClientRequestHandler.java
+++ src/main/java/com/bytethrasher/turbine/request/ApacheHttpClientRequestHandler.java
@@ -2,15 +2,32 @@ package com.bytethrasher.turbine.request;
 
 import com.bytethrasher.turbine.request.domain.Response;
 import lombok.Builder;
+import lombok.SneakyThrows;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 
-import java.nio.charset.StandardCharsets;
-
-@Builder
 public class ApacheHttpClientRequestHandler implements RequestHandler {
 
+    private static final String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            + " (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
+
+    private final HttpClient httpClient;
+
+    @Builder
+    public ApacheHttpClientRequestHandler(final String userAgent) {
+        final String realUserAgent = userAgent == null ? DEFAULT_USER_AGENT : userAgent;
+
+        httpClient = HttpClientBuilder.create()
+                .setUserAgent(realUserAgent)
+                .build();
+    }
+
     @Override
+    @SneakyThrows //TODO: This SneakyThrows can be very bad here (kill the app)
     public Response doRequest(final String location) {
-        // TODO: Do the request here.
-        return new Response(200, "hello".getBytes(StandardCharsets.UTF_8));
+        return httpClient.execute(new HttpGet(location), response ->
+                new Response(response.getCode(), EntityUtils.toByteArray(response.getEntity())));
     }
 }
--- src/main/java/com/bytethrasher/turbine/response/writer/WARCResponseWriter.java
+++ src/main/java/com/bytethrasher/turbine/response/writer/WARCResponseWriter.java
@@ -4,21 +4,48 @@ import com.bytethrasher.turbine.request.domain.Response;
 import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.netpreserve.jwarc.HttpResponse;
+import org.netpreserve.jwarc.MediaType;
+import org.netpreserve.jwarc.WarcResponse;
+import org.netpreserve.jwarc.WarcWriter;
 
+import java.time.Instant;
 import java.util.concurrent.BlockingQueue;
 
 @Slf4j
-@Builder
 public class WARCResponseWriter implements ResponseWriter {
 
+    // TODO: Add rolling...
+    private final WarcWriter writer;
+
+    @Builder
+    @SneakyThrows
+    public WARCResponseWriter() {
+        writer = new WarcWriter(System.out);
+    }
+
+    //TODO: Isn't it possible that the main thread shuts down faster than everything is written to file?
     @Override
     @SneakyThrows
     public void writeResponsesFromQueue(final BlockingQueue<Response> queue) {
         while (true) {
             final Response response = queue.take();
 
-            //TODO: write properly
-            log.info(response.toString());
+            // TODO: Save request as well
+
+            WarcResponse warcResponseRecord = new WarcResponse.Builder("asd")
+                    .date(Instant.now())
+                    .body(
+                            //TODO: Support reason
+                            new HttpResponse.Builder(response.statusCode(), "asd")
+                                    //TODO: Support media type
+                                    .body(MediaType.parse("application/html"), response.content())
+                                    .build()
+                    )
+                    // TODO concurrentTo
+                    //.concurrentTo(request.id())
+                    .build();
+            writer.write(warcResponseRecord);
         }
     }
 }
